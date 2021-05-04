805e2335101782514bf4024ad327799dbb4698cb
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-05-04 18:41:56 +0530

    update Talaria master.sh
    edit MMproduct to Talaria and change mailam to Talaria

--- master.sh
+++ master.sh
@@ -8,14 +8,14 @@ cd $HOME/Satriaana/MailMan/temp
 
 orgName=$(echo $MMREPO | sed  's/\/.*//g')
 
-git clone --quiet https://github.com/"$orgName"/MMproduct.git 
+git clone --quiet https://github.com/"$orgName"/Talaria.git 
 
 echo Connecting to Satriaana Repository...
 
 echo "Setting Up Working Directory..."
 #sleep 10
 
-cd mailman
+cd Talaria
 cp installer.sh $HOME/Satriaana/MailMan/
 chmod +x base.sh
 
